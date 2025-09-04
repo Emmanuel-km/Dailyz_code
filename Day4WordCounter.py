e1450fdabd7b773f31f36a6bec7dded565ad1a38
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-04 23:09:30 +0300

    this is the first phase of word counting
    This is my first try hope to continue it to make it workable and neet

---
+++ Day4WordCounter.py
@@ -0,0 +1,6 @@
+#have a text to count the words
+text="this is my daily code I passionatly write code everyday to enhance my coding skils walk with me in this path"
+#using split() method
+#first trial
+for i in text:
+  text=text.split(" ")
