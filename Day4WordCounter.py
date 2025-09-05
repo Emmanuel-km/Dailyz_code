b8cfa41db7d36f45b97cc3156eac66d485b34792
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-05 23:13:42 +0300

    good
    so strip() is a method close to split but this only removes white spaces

--- Day4WordCounter.py
+++ Day4WordCounter.py
@@ -1,6 +1,15 @@
 #have a text to count the words
-text="this is my daily code I passionatly write code everyday to enhance my coding skils walk with me in this path"
+My_text="this is my daily code I passionatly write code everyday to enhance my coding skils walk with me in this path"
 #using split() method
-#first trial
-for i in text:
-  text=text.split(" ")
+#split function saves the values in a list so for loop will be used
+sentence=My_text.split(" ")
+#declaring a count variable to count words in the text
+count=0
+for i in sentence:
+    count+=1
+#printing the outcome
+print(f"The number of words present in text are {count}")
+
+
+ 
+
