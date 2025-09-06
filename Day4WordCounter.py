8ce48ed78e8655697d07a21316c4163d5b4c9515
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-06 18:39:01 +0300

    updating to user input
    This code update generally takes user input cleans it by strip to remove extra white space. Any errors is cought then the code is restarted without crashing

--- Day4WordCounter.py
+++ Day4WordCounter.py
@@ -1,6 +1,16 @@
 #have a text to count the words
-My_text="this is my daily code I passionatly write code everyday to enhance my coding skils walk with me in this path"
+#My_text="this is my daily code I passionatly write code everyday to enhance my coding skils walk with me in this path"
+#using the strip method from user input
+#the try block is for exception handling
+#the function.strip removes white space at the ends
+def user_input():
+    try:
+        My_text=input("enter text to be counted).strip()
+     except:
+         user_input()
+user_input()
 #using split() method
+
 #split function saves the values in a list so for loop will be used
 sentence=My_text.split(" ")
 #declaring a count variable to count words in the text
