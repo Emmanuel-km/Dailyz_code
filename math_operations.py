9c3619ca4383207169a9ccc1c0f27c1f6a2b5e1f
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-08 16:09:46 +0300

    a elaborated way to import modules in python

---
+++ math_operations.py
@@ -0,0 +1,12 @@
+import math
+import random
+def addition(x,y):
+    return print(x+y)
+def subtraction(x,y):
+    return print(x-y)
+def multiplication(x,y):
+    return print(x*y)
+def division(x,y):
+    a=(x/y)
+    a=math.floor(a)
+    return print(a)
