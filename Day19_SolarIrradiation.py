082e5a1dbceae20ffc91a4bdd46c72986dc9beac
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-19 22:47:50 +0300

    tresh: using csv files

--- Day19_SolarIrradiation.py
+++ Day19_SolarIrradiation.py
@@ -2,3 +2,15 @@
 #opening csv files cleaning, computing monthly average over 
 #the past 30 years monthly
 #summerizing the data
+
+#opening csv files
+#csv files are commer separated values
+
+#so first;
+
+import csv
+with open('vim.csv','+w',newline='') as file:
+    spamwriter = csv.writer(file, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
