0e3245608b83b86c819b0b976914164a08de942f
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-21 16:18:24 +0300

    Updated data analysis

--- Day21_NumpyInSolarIrradiation.py
+++ Day21_NumpyInSolarIrradiation.py
@@ -1,7 +1,9 @@
 #using numpy
 import numpy as np
 
-#text=np.loadtxt('homeData.csv',delimiter=',')
+#text=np.loadtxt('homeData.csv',delimiter=",",encoding='UTF-8',skiprows=0,dtype=str)
 
-text=np.genfromtxt('homeData.csv',delimiter=',')
+#another method
+
+text=np.genfromtxt('homeData.csv',delimiter=',',dtype=str,skip_header=1)
 print(text)
