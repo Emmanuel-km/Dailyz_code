81e7fe4a9613129da0972e08bbc5058660a6fef9
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-22 23:30:50 +0300

    more workings with pandas: series

--- Day22_PandasBasics.py
+++ Day22_PandasBasics.py
@@ -22,6 +22,10 @@ def series():
         ser=pd.Series(np.random.randn(4))
         print(ser)
 
+        #indexing and acessing the third element
+        ser[2]=4.765
+        
+        print(ser)
         #an index can be used as a string object
         import calendar as cal
         MonthName=[cal.month_name[i] for i in np.arange(1,6)]
@@ -37,6 +41,33 @@ def series():
         curr_dict={"US":"dolla","UK":"pound","GERMANY":"euro","MEXICO":"peso","NIGERIA":"naira","CHINA":"yuan","JAPAN":"yen"}
         curr_series=pd.Series(curr_dict)
         print(curr_series)
+        #so series has methods head and tail 
+        #the method head() accssesses the value(s) specified found on top of
+        #  the list specified selection
+        #the method tail() accesses the value(s) specified found
+        #  below the specified selection
+
+        #example is 1
+        #head(1) will only access the first value in the sequence
+        #tail(1) will only access the last element in the sequence
+
+        print(curr_series.head(1))
+        print(curr_series.tail(1))
+
+        #method dtype
+        print(curr_series.dtype)
+
+        #method describe
+        #this describes the whole series
+        '''
+        object
+        count         7
+        unique        7
+        top       dolla
+        freq          1
+        dtype: object
+        '''
+        print(curr_series.describe())
     python_dictionary()
 
     #3.scalar values
