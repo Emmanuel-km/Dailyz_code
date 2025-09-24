d4a10e8be8281960864556f3644ebd204d6531b7
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-09-24 21:30:20 +0300

    recaping on numpy with dimensions

--- Day24_numpyPandasMatplotlib.py
+++ Day24_numpyPandasMatplotlib.py
@@ -11,5 +11,23 @@ def showploting():
     plt.show()
 #showploting()
 
+x=np.arange(0,40,4)
+#print(x)
+ 
+x=np.arange(10.4)
+#print(x)
 
+x=np.linspace(0,40,num=11,endpoint=True,retstep=True)
+#print(x)
 
+x=np.array(78)
+#print(x)
+#print(np.ndim(x))
+
+x=np.array([3,5,7,8,9,1,2])
+
+x=np.array([[3,6,9,12,15,18,21,24,27,30],[9,18,27,36,45,54,63,72,81,90]])
+#print(x)
+
+x=np.array([[[2,4,6],[1,2,3]],[[3,6,9],[6,12,18]],[[4,8,12],[8,16,24]]])
+print(x)
