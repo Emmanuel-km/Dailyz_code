ecbf7024fb0d1d2723cf5734337d5b3a473ea084
Emmanuel Kioko <kiokoemmanuel027@gmail.com>
2025-10-27 18:05:12 +0000

    data cleaning

--- Day57_AppliedSeabornpandas.py
+++ Day57_AppliedSeabornpandas.py
@@ -4,3 +4,16 @@ import seaborn as sns
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+# Data extraction and cleaning
+"""
+ in this case the home data CSV 
+ file will be looked upon and 
+ operations on data will  be handled by
+ pandas
+"""
+
+df=pd.read_csv('homeData.csv')
+#droping null values
+df.dropna(inplace=True)
+#data
+data=df[['wind_speed','time']]
