4538d4457900402301714ba5fa49f636736ff6df
Emmanuel Kioko <kiokoemmanuel027@gmail.com>
2025-11-19 23:40:25 +0300

    numpy library

--- progress.py
+++ progress.py
@@ -1,5 +1,9 @@
 """
- over the past 2 months studying towards research assistant, 
+over the past 2 months studying towards research assistant, 
 
+progress made
+
+started off with python basics
+then numpy libraries 
 
 """
